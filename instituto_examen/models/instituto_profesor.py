483e8495f5e7a7f1b9419e6219277f18685e9879
SergioDGr <sergiodanielgroppa@gmail.com>
2024-02-05 12:08:12 +0100

    ADD profesor

---
+++ instituto_examen/models/instituto_profesor.py
@@ -0,0 +1,30 @@
+from odoo import fields, models
+
+class InstitutoProfesor(models.Model):
+    _name = "instituto.profesor"
+    _description = "Tabla de las diferentes profesores"
+
+    # Para ver las tablas:
+    # sudo su postgres
+    # psql
+    # \c dm2
+    # \d instituto_empresa
+
+    # Tiene que tener si o si un atributo name porque sino coge el id
+    name = fields.Char(required=True, string='Nombre')
+    apellidos = fields.Char(required=True, string='Apellidos')
+    email = fields.Char(string='Email')
+    depar = fields.Selection(
+        string='Departamento',
+        selection=[('informática', 'Informática'), ('comercio', 'Comercio'), ('marketing', 'Marketing'),
+                   ('administración', 'Administración')], default="informática", help="Departamento")
+
+    # Si en el xml ponemos: <button name="accion_servidor" type="object" string="Ayuda"/>
+    # Nos aparece un botón con un hipervínculo si descomentamos además la función:
+
+    # def accion_servidor(self):
+    #     return {
+    #         "type": "ir.actions.act_url",
+    #         "url": "https://odoo.com",
+    #         "target": "self",
+    #     }
