8e6555c247efefd15f9186a17158af2d378aabb4
SergioDGr <sergiodanielgroppa@gmail.com>
2024-02-05 12:36:19 +0100

    change

--- sergio_groppa/__manifest__.py
+++ sergio_groppa/__manifest__.py
@@ -2,7 +2,7 @@
 # Part of Odoo. See LICENSE file for full copyright and licensing details.
 
 {
-    'name': 'Instituto',
+    'name': 'Feb24Sergio',
     'version': '1.0',
     'category': 'Technical',
     'description': 'Módulo instituto',
